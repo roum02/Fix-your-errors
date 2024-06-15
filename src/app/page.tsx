425a7dde0b1632a707468f9094f2bec75511b889
roum02 <roum02@ewhain.net>
2024-06-16 01:26:13 +0900

    Add data [main]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,16 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
  <main>
+     <menu>
+         <span>real-time</span>
+         <span>submit</span>
+     </menu>
    <h1> Fix your Typing Error here! </h1>
-     <textarea/>
+     <p>origin</p>
+     <textarea />
      <button>submit</button>
+     <p>fixed version</p>
+     <textarea/>
  </main>
   );
 }
