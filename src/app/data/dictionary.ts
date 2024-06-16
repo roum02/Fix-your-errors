9960747df90e8309c495a2d45680ddc4affa3dd7
roum02 <roum02@ewhain.net>
2024-06-16 16:51:45 +0900

    Add character object data [main]

--- src/app/data/dictionary.ts
+++ src/app/data/dictionary.ts
@@ -1,4 +1,25 @@
-export const koreanDictionary = new Map([['p','ㅂ'], ['Q', 'ㅃ'], ['w', 'ㅈ'], ['W','ㅉ'],
+const singleConsonants = {
+    'q': 'q','w':'w','e':'e','r':'r','t':'t',
+    'a':'a','s':'s','d':'d','f':'f','g':'g',
+    'z':'z','x':'x','c':'c','v':'v'
+}
+
+const doubleConsonants = {
+    'Q':'Q','W':'W','E':'E','R':'R','T':'T',
+}
+
+const singleVowels = {
+    'y':'y','u':'u','i':'i','o':'o','p':'p',
+    'h':'h','j':'j','k':'k','l':'l',
+    'b':'b','n':'n','m':'m'
+}
+
+const doubleVowels = {
+    'O':'O','P':'P'
+}
+
+export const koreanDictionary = new Map([
+    ['p','ㅂ'], ['Q', 'ㅃ'], ['w', 'ㅈ'], ['W','ㅉ'],
     ['e','ㄷ'], ['E','ㄸ'], ['r','ㄱ'], ['R','ㄲ'], ['t','ㅅ'],
     ['T','ㅆ'], ['y','ㅛ'], ['u','ㅕ'], ['i','ㅑ'], ['o','ㅐ'],
     ['p','ㅔ'], ['a','ㅁ'], ['s','ㄴ'], ['d','ㅇ'], ['f','ㄹ'],
@@ -11,3 +32,10 @@ export const koreanDictionary = new Map([['p','ㅂ'], ['Q', 'ㅃ'], ['w', 'ㅈ']
     ['X','ㅌ'], ['C','ㅊ'], ['V','ㅍ']
 ])
 
+export const consonants = {
+    ...singleConsonants, ...doubleConsonants
+}
+
+export const vowels = {
+    ...singleVowels, ...doubleVowels
+}
