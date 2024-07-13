bef1d1ced2d1b2daf8b57d15e02267902d2f21ef
roum02 <roum02@ewhain.net>
2024-07-14 00:55:04 +0900

    Add object of 이중모음 이중자음 [feature/makeFunction]

--- src/app/utils/translate.ts
+++ src/app/utils/translate.ts
@@ -1,5 +1,42 @@
 import { CHOSEONG, JONGSEONG, JUNGSEONG, KOREAN_DICTIONARY} from '../data/dictionary'
 
+/** TODO this file should be moved */
+const jongseongMap: { [key: string]: string } = {
+    'ㄱㄱ': 'ㄲ',
+    'ㄱㅅ': 'ㄳ',
+    'ㄴㅈ': 'ㄵ',
+    'ㄴㅎ': 'ㄶ',
+    'ㄹㄱ': 'ㄺ',
+    'ㄹㅁ': 'ㄻ',
+    'ㄹㅂ': 'ㄼ',
+    'ㄹㅅ': 'ㄽ',
+    'ㄹㅌ': 'ㄾ',
+    'ㄹㅍ': 'ㄿ',
+    'ㄹㅎ': 'ㅀ',
+    'ㅂㅅ': 'ㅄ'
+};
+
+const jungseongMap: { [key: string]: string } = {
+    'ㅗㅏ': 'ㅘ',
+    'ㅗㅐ': 'ㅙ',
+    'ㅗㅣ': 'ㅚ',
+    'ㅜㅓ': 'ㅝ',
+    'ㅜㅔ': 'ㅞ',
+    'ㅜㅣ': 'ㅟ',
+    'ㅡㅣ': 'ㅢ'
+};
+
+const makeJongseongList = ( charList: string[]): string => {
+    const key = charList.join('');
+    return jongseongMap[key] || charList[0];
+}
+
+const makeJungseongList = (charList: string[]): string => {
+    const key = charList.join('');
+    return jungseongMap[key] || charList[0];
+}
+
+
 /** get a korean character matched an english character*/
 export const handleEnglishCharacterMatch = (englishText: string) => {
     let koreanTextArray: string[] = [];
@@ -23,38 +60,72 @@ const combineKoreanCharacter = (characterArray: string[]) => {
     const jungseongIndex = JUNGSEONG.get(jungseong) as number;
     const jongseongIndex = JONGSEONG.get(jongseong) || 0;
 
-    // console.log(choseong, jungseong, jongseong)
-
     const hangulChar =  String.fromCharCode(0xAC00 + (choseongIndex * 21 * 28) + (jungseongIndex * 28) + jongseongIndex)
-    console.log("hangulChar", hangulChar)
     return hangulChar;
 }
 
 /** combine a korean character to make a korean word */
-export const handleKoreanWord = (characterArray: string[]) => {
+export const handleKoreanWord = (koreanCharacterArray: string[]) => {
     let hangulString = '';
-    let buffer: string[] = [];
+    let words: string[] = [];
+    let start = 0;
 
     const isJungseong = (char: string) => JUNGSEONG.has(char);
     const isChoseong = (char: string) => CHOSEONG.has(char);
     const isJongseong = (char: string) => JONGSEONG.has(char);
 
+    const isEndOfWord = (char: string, nextChar: string | undefined): boolean => {
+        return (isChoseong(char) && nextChar && isJungseong(nextChar)) ||
+            (!isChoseong(char) && (!nextChar || !isJungseong(nextChar)));
+    };
 
-    characterArray.forEach((char, index)=> {
-        buffer.push(char);
 
-        if (buffer.length === 3 && isChoseong(buffer[0]) && isJungseong(buffer[1]) && isJongseong(buffer[2])) {
-            hangulString += combineKoreanCharacter(buffer);
-            buffer = [];
+    words = koreanCharacterArray.reduce((acc: string[], char, i, array) => {
+        const nextChar = array[i + 1];
+        if (i === array.length - 1) {
+            acc.push(array.slice(start).join(''));
+        } else if (isEndOfWord(char, nextChar)) {
+            acc.push(array.slice(start, i).join(''));
+            start = i;
         }
-    });
+        return acc;
+    }, []);
 
-    if (buffer.length > 0) {
-        while (buffer.length < 3) {
-            buffer.push('');
+
+    const processWord = (word: string): string => {
+        let wordArray = word.split('');
+
+        const processJungseong = (arr: string[], startIndex: number): void => {
+            if (isJungseong(arr[startIndex]) && isJungseong(arr[startIndex + 1])) {
+                let original = arr[startIndex];
+                arr[startIndex] = makeJungseongList(arr.slice(startIndex, startIndex + 2));
+                if (original !== arr[startIndex]) {
+                    arr.splice(startIndex + 1, 1);
+                }
+            }
+        };
+
+        const processJongseong = (arr: string[], startIndex: number): void => {
+            if (isJongseong(arr[startIndex]) && isJongseong(arr[startIndex + 1])) {
+                let original = arr[startIndex];
+                arr[startIndex] = makeJongseongList(arr.slice(startIndex, startIndex + 2));
+                if (original !== arr[startIndex]) {
+                    arr.splice(startIndex + 1, 1);
+                }
+            }
+        };
+
+        if (wordArray.length > 2 && isChoseong(wordArray[0]) && isJungseong(wordArray[1])) {
+            processJungseong(wordArray, 1);
+            if (wordArray.length >= 4) {
+                processJongseong(wordArray, 2);
+            }
         }
-        hangulString += combineKoreanCharacter(buffer);
-    }
-    console.log("final", hangulString)
-    return hangulString;
+
+        return wordArray.join('');
+    };
+
+    words = words.map(processWord);
+
+    return '';
 }
