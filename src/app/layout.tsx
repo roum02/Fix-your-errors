ec34344615fbbfc582c65c0cb7efb99350313368
roum02 <roum02@ewhain.net>
2024-07-28 22:51:51 +0900

    Add pwa settings [feature/pwa]

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+    <head>
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="manifest" href="/manifest.json" />
+      <title>en2han</title>
+      <script type="module">
+        {`
+            import 'https://cdn.jsdelivr.net/npm/@pwabuilder/pwaupdate';
+            const el = document.createElement('pwa-update');
+            document.body.appendChild(el);
+          `}
+      </script>
+    </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
