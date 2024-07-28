c97d727da50f5ae777168364c04fb79e2f78396b
roum02 <roum02@ewhain.net>
2024-07-29 00:44:45 +0900

    Add client script component [feature/pwa]

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Script from 'next/script';
+import ClientScript from "@/clientScript";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,15 +17,16 @@ export default function RootLayout({
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="manifest" href="/manifest.json" />
       <title>en2han</title>
-      <Script
-          strategy="afterInteractive"
-          type="module"
-          src="https://cdn.jsdelivr.net/npm/@pwabuilder/pwaupdate"
-          onLoad={() => {
-            const el = document.createElement('pwa-update');
-            document.body.appendChild(el);
-          }}
-      />
+      <ClientScript />
+      {/*<Script*/}
+      {/*    strategy="afterInteractive"*/}
+      {/*    type="module"*/}
+      {/*    src="https://cdn.jsdelivr.net/npm/@pwabuilder/pwaupdate"*/}
+      {/*    onLoad={() => {*/}
+      {/*      const el = document.createElement('pwa-update');*/}
+      {/*      document.body.appendChild(el);*/}
+      {/*    }}*/}
+      {/*/>*/}
     </head>
       <body className={inter.className}>{children}</body>
     </html>
---
+++ src/clientScript.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import Script from 'next/script';
+import { useEffect } from 'react';
+
+export default function ClientScript() {
+    useEffect(() => {
+        const el = document.createElement('pwa-update');
+        document.body.appendChild(el);
+    }, []);
+
+    return (
+        <Script
+            strategy="afterInteractive"
+            type="module"
+            src="https://cdn.jsdelivr.net/npm/@pwabuilder/pwaupdate"
+        />
+    );
+}
