209df85baa029e8acc9830d10f775976bf0143ef
roum02 <roum02@ewhain.net>
2024-08-01 23:20:06 +0900

    Fix directory [feature/pwa]

--- src/app/clientScript.tsx
+++ src/app/clientScript.tsx
@@ -6,20 +6,6 @@ import { useEffect } from 'react';
 export default function ClientScript() {
 
     useEffect(() => {
-        if ('serviceWorker' in navigator) {
-            window.addEventListener('load', () => {
-                navigator.serviceWorker.register('/pwabuilder-sw.js')
-                    .then(registration => {
-                        console.log('SW registered: ', registration);
-                    })
-                    .catch(registrationError => {
-                        console.log('SW registration failed: ', registrationError);
-                    });
-            });
-        }
-    }, []);
-
-    useEffect(() => {
         const el = document.createElement('pwa-update');
         document.body.appendChild(el);
         return () => {
